ef1f1945e2472bd36e05be9d49a0dda155f588fd
ALKHWARIZMI-PC\abelm <abelmanuellugtujr1996@gmail.com>
2023-05-21 19:47:21 +0800

    ✨ feat: Dev Tools

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import './App.css';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import { SuperHeroesPage } from './components/SuperHeroesPage';
@@ -9,26 +10,25 @@ function App() {
   return (
     <QueryClientProvider client={new QueryClient()}>
       <BrowserRouter>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/super-heroes">Traditional Super Heroes</Link>
-              </li>
-              <li>
-                <Link to="/rq-super-heroes">RQ Super Heroes</Link>
-              </li>
-            </ul>
-          </nav>
-          <Routes>
-            <Route path="/super-heroes" element={<SuperHeroesPage />} />
-            <Route path="/rq-super-heroes" element={<RQSuperHeroesPage />} />
-            <Route path="/" element={<HomePage />} />
-          </Routes>
-        </div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/super-heroes">Traditional Super Heroes</Link>
+            </li>
+            <li>
+              <Link to="/rq-super-heroes">RQ Super Heroes</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/super-heroes" element={<SuperHeroesPage />} />
+          <Route path="/rq-super-heroes" element={<RQSuperHeroesPage />} />
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+        <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
       </BrowserRouter>
     </QueryClientProvider>
   );
